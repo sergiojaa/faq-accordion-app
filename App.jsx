534e705aeb8397b53a1b4caaf3a80e5dd0d9c4c1
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-10-31 00:32:45 +0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import DivsHide from "./components/DivsHide"
 import Rate from "./components/Rate"
-
+sss
 
   function App() {
    const [show, setShow] = useState(true)
